e70d5243a565186119d434501101dd5b04efa299
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2026-01-05 21:23:12 +0100

    Set CQ or ITU to null

--- application/views/logbookadvanced/edit.php
+++ application/views/logbookadvanced/edit.php
@@ -62,7 +62,7 @@
 
 		<!-- CQ Zone -->
 		<select style="display:none" class="form-select form-select-sm w-auto" id="editCqz" name="cqz" required>
-			<option value="">-</option>
+			<option value="null">-</option>
 			<?php
 			for ($i = 1; $i <= 40; $i++) {
 				echo '<option value="' . $i . '">' . $i . '</option>';
@@ -72,7 +72,7 @@
 
 		<!-- ITU Zone -->
 		<select style="display:none" class="form-select form-select-sm w-auto" id="editItuz" name="ituz" required>
-			<option value="">-</option>
+			<option value="null">-</option>
 			<?php
 			for ($i = 1; $i <= 90; $i++) {
 				echo '<option value="' . $i . '">' . $i . '</option>';
