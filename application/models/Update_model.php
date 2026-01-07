e0cacea7396f28033c97a1bf2e103436ef46ee53
phl0 <github@florian-wolters.de>
2026-01-07 16:16:20 +0100

    Remove accidentially checked in line of code

--- application/models/Update_model.php
+++ application/models/Update_model.php
@@ -643,7 +643,6 @@ class Update_model extends CI_Model {
 		$response = curl_exec($curl);
 
 		$xml = @simplexml_load_string($response);
-      $xml = false;
 
 		if ($xml === false) {
 			log_message('error', 'vuccgrids.dat update from primary location failed.');
