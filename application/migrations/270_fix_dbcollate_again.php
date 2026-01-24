a5a34f1848ade6ded899ab22466c2597298484cd
kc9uhi <21296099+kc9uhi@users.noreply.github.com>
2026-01-24 16:10:18 -0700

    error handling

--- application/migrations/270_fix_dbcollate_again.php
+++ application/migrations/270_fix_dbcollate_again.php
@@ -14,14 +14,22 @@ class Migration_fix_dbcollate_again extends CI_Migration
 			'vuccgrids'
 		);
 		foreach ($tables as $table) {
-			$this->db->query('ALTER TABLE ' . $table . ' CONVERT TO CHARACTER SET ' . $this->db->char_set . ' COLLATE ' . $this->db->dbcollat); // fix existing tables that haven't already been fixed
+			$this->dbtry('ALTER TABLE ' . $table . ' CONVERT TO CHARACTER SET ' . $this->db->char_set . ' COLLATE ' . $this->db->dbcollat); // fix existing tables that haven't already been fixed
 		}
-		$this->db->query('ALTER DATABASE `' . $this->db->database . '` CHARACTER SET ' . $this->db->char_set . ' COLLATE ' . $this->db->dbcollat); // fix the database default
+		$this->dbtry('ALTER DATABASE `' . $this->db->database . '` CHARACTER SET ' . $this->db->char_set . ' COLLATE ' . $this->db->dbcollat); // fix the database default
 	}
 
 	public function down()
 	{
 		// Not Possible
+	}
+
+	function dbtry($what) {
+		try {
+			$this->db->query($what);
+		} catch (Exception $e) {
+			log_message("error", "Error setting character set/collation: ".$e." // Executing: ".$this->db->last_query());
+		}
 	}
 }
 ?>
