28cf4b2f6b2146b488aa79930fec04a37b8a6491
sm6srw <sm6srw@arrl.net>
2026-03-29 08:43:29 -0400

    Change to slots

--- application/views/awards/dxcc/index.php
+++ application/views/awards/dxcc/index.php
@@ -313,7 +313,7 @@
 				}
 				if ($posted_band != 'SAT') {
 					echo '<td><b>' . __("Total (ex SAT)") . '</b></td>';
-					echo '<td><b>' . __("Band slots (ex SAT)") . '</b></td>';
+					echo '<td><b>' . __("Slots") . '</b></td>';
 				}
 				echo '
 	</tr>
