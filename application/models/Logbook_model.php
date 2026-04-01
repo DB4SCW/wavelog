36a043c1bfb926ca0a88669465275780782a0912
phl0 <github@florian-wolters.de>
2026-04-01 17:29:01 +0200

    Fix zone in case as well

--- application/models/Logbook_model.php
+++ application/models/Logbook_model.php
@@ -556,7 +556,7 @@ class Logbook_model extends CI_Model {
 				break;
 			case 'ITUZone':
 				$this->db->where('COL_ITUZ', $searchphrase);
-				if ($band == 'SAT' && $type == 'ITU') {
+				if ($band == 'SAT' && $type == 'ITUZone') {
 					if ($sat != 'All' && $sat != null) {
 						$this->db->where("COL_SAT_NAME", $sat);
 					}
